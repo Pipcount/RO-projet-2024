e0c392aa775c78a21ca5630b99040ee6775cc9f0
Pipcount <nico@janke-online.net>
2024-04-26 09:18:35 +0200

    added calculate dist

--- cholet.py
+++ cholet.py
@@ -16,8 +16,20 @@ def load_data(folder : str) -> dict:
 
 def calculate_total_dist(data: dict) -> int:
     total_dist = 0
-     
-
+    init_sol = data["init_sol_Cholet_pb1_bis.pickle"]     
+    dist_matrix = data["dist_matrix_Cholet_pb1_bis.pickle"]
+    print(init_sol)
+    print()
+    print(dist_matrix)
+    print()
+    for i in range(len(init_sol)):
+        node = init_sol[i]
+        total_dist += dist_matrix[node][node]
+        if i < len(init_sol) - 1:
+            next_node = init_sol[i + 1]
+            total_dist += dist_matrix[node][next_node]
+    print(total_dist)
+    return total_dist
 
 
 if __name__ == "__main__":
